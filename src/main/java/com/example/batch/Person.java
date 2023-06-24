5dd12422f34caaa9a81fffc2b3d00000c28de150
Hoseok Seong <shsshs0125@naver.com>
2023-06-24 20:55:25 +0900

    Batch 작업 후 출력까지 확인 완료

--- src/main/java/com/example/batch/Person.java
+++ src/main/java/com/example/batch/Person.java
@@ -5,6 +5,9 @@ public class Person {
     private String lastName;
     private String firstName;
 
+    public Person() {
+    }
+
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
